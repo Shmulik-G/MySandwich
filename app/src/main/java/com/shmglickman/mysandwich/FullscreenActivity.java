d533854cfdc5f928186c3ea97c9477113bfbecc3
Shmulik-G <Glickmanso@gmail.com>
2019-03-26 21:09:11 +0200

    Add New Remark

--- app/src/main/java/com/shmglickman/mysandwich/FullscreenActivity.java
+++ app/src/main/java/com/shmglickman/mysandwich/FullscreenActivity.java
@@ -8,6 +8,8 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
+/*   This is A Remark */
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
